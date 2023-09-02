381d8b02573c54a0de4bcfbb957f92c44b7ee3b7
deseretpatricia <deseretprs@gmail.com>
2023-09-02 16:06:20 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -2,6 +2,7 @@ const button = document.getElementById("botonSaludo");
 const container = document.getElementById("container");
 
 button.onclick = function () {
+    event.stopPropagation();
     alert("Hola!");
 };
 
